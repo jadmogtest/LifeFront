8b0ad6cbdf0cd80286fdde9fc8544be36cb53243
MandyTrl <mandy.thorel@gmail.com>
2022-05-26 10:59:19 +0200

    commit pr merhge en local

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -223,7 +223,16 @@ function MapScreen(props) {
             onRequestClose={() => {
               setModalVisible(!modalVisible);
             }}
+            backdropColor="#B4B3DB"
+            backdropOpacity={0.8}
+            animationIn="zoomInDown"
+            animationOut="zoomOutUp"
+            animationInTiming={600}
+            animationOutTiming={600}
+            backdropTransitionInTiming={600}
+            backdropTransitionOutTiming={600}
           >
+            
             <View style={styles.modalView}>
               <Text
                 style={{
